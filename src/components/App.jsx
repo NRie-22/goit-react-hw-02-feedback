09e8dfeb7ee96b34204dca539f4ae23254d75455
Nataliya Rieger <artgallery2018@gmx.ch>
2023-06-19 20:54:57 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -59,3 +59,20 @@ class App extends Component {
 }
 
 export default App;
+
+export const AnotherApp = () => {
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101',
+      }}
+    >
+      React homework template
+    </div>
+  );
+};
